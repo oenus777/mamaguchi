c1c1226cb0352c55376d8e3093aedadc8d613132
suneokun <a>
2020-03-16 08:33:25 +0000

    index, about.html update1

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,15 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
 require("turbolinks").start()
+require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
+
+
+import 'bootstrap'
+import '@fortawesome/fontawesome-free/js/all'
+import '../src/application.scss'
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
