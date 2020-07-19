e426417e5edcd3770ac1f56631cf2db7998aa98e
suneokun <a>
2020-07-19 08:25:35 +0000

    [Create] rpsec type:requests likes [ci skip]

---
+++ spec/requests/likes_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+RSpec.describe 'Like', type: :request do
+    let!(:post1) { create(:post) }
+
+    describe 'POST #create' do
+        context '未ログイン時' do
+            it 'リクエストが不成功' do
+                expect do
+                    post likes_path,params: { post_id: 1 }
+                end.to change(Like, :count).by(0)
+                expect(response).to have_http_status 302
+                expect(response).to redirect_to new_user_session_path
+            end
+        end
+        context 'ログイン時' do
+            it 'リクエストが成功' do
+                sign_in(post1.user)
+                expect do
+                    post likes_path,params: { post_id: 1 }
+                end.to change(Like, :count).by(1)
+                expect(response).to have_http_status 302
+                expect(response).to redirect_to root_path
+            end
+        end
+    end
+    describe 'DELETE #destroy' do
+        context '未ログイン時' do
+            it 'リクエストが不成功' do
+                expect do
+                    delete like_path(post1), params: { post_id: 1 }
+                end.to change(Like, :count).by(0)
+                expect(response).to have_http_status 302
+                expect(response).to redirect_to new_user_session_path
+            end
+        end
+        context 'ログイン時' do
+            it 'リクエストが成功' do
+                sign_in(post1.user)
+                expect do
+                    post likes_path,params: { post_id: 1 }
+                end.to change(Like, :count).by(1)
+                expect do
+                    delete like_path(post1),params: { post_id: 1 }
+                end.to change(Like, :count).by(-1)
+                expect(response).to have_http_status 302
+                expect(response).to redirect_to root_path
+            end
+        end
+    end
+end
