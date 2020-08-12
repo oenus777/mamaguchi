aa28434b781680720b111f00513608428763ee39
suneokun <a>
2020-08-12 14:11:11 +0000

    [Update] users#registration controller [ci skip]

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -40,13 +40,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   #protected
 
-  #def update_resource(resource, params)
-　#  resource.update_without_password(params)
-　#end
-　
-　#def after_update_path_for(resource)
-  #  edit_user_registration_path(resource)
-  #end
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
   #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
